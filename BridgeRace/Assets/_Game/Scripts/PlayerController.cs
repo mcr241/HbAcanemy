2200dc9f2ebf0be218a33f07811ef545222b6f78
Hoang Anh <tinhban.hal@gmail.com>
2022-10-10 21:22:32 +0700

    ~ player controller

--- BridgeRace/Assets/_Game/Scripts/PlayerController.cs
+++ BridgeRace/Assets/_Game/Scripts/PlayerController.cs
@@ -6,6 +6,8 @@ public class PlayerController : MonoBehaviour
 {
     [SerializeField] float speed;
     [SerializeField] Rigidbody rigidbody;
+    [SerializeField] Transform checkGround;
+    [SerializeField] LayerMask layerGround;
     Vector3 fisrtPointJoystick;
     bool isOnJoystick;
     private void Update()
@@ -15,10 +17,11 @@ public class PlayerController : MonoBehaviour
             fisrtPointJoystick = Input.mousePosition;
             isOnJoystick = true;
         }
-
+        
         if (isOnJoystick)
         {
-            Move();
+            if (CheckGround())
+                Move();
         }
 
         if (Input.GetMouseButtonUp(0))
@@ -27,12 +30,20 @@ public class PlayerController : MonoBehaviour
         }
     }
 
+    bool CheckGround()
+    {
+        return Physics.Raycast(checkGround.position, Vector2.down, 1.1f, layerGround);
+    }
+
     private void Move()
     {
         Vector3 distan = new Vector3(Input.mousePosition.x - fisrtPointJoystick.x, 0, Input.mousePosition.y - fisrtPointJoystick.y);
         if (distan.magnitude > 0.5f)
         {
             rigidbody.velocity = new Vector3((distan.normalized * speed).x, rigidbody.velocity.y, (distan.normalized * speed).z);
+            float angle = Vector2.Angle(Vector2.up, Input.mousePosition);
+         
+            transform.GetChild(0).rotation = Quaternion.Euler(0, angle, 0);
         }
     }
 
